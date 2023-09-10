c41b240a4fb5dcb20b6515340f288d4d499e57ea
Ahmed AlMoselhy <ahmedalmoselhy.slm@gmail.com>
2023-09-10 17:25:43 +0300

    add basic auth translation

---
+++ lang/en/auth.php
@@ -0,0 +1,12 @@
+<?
+
+return [
+    'login' => 'Login',
+    'register' => 'Register',
+    'logout' => 'Logout',
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'confirm_password' => 'Confirm password',
+    'name' => 'Name',
+];
