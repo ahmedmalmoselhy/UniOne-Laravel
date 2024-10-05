83395d4c9762297ea98cbc682353654c768e3566
Ahmed AlMoselhy <ahmed.almoselhy@qeema.net>
2024-10-06 00:06:13 +0300

    Add constants

--- app/Helpers/Constants.php
+++ app/Helpers/Constants.php
@@ -7,9 +7,15 @@ class Constants
     /**
      * ENUM Constants
      */
+
+    // All user types
     public const USER_TYPES = ['admin', 'employee', 'student', 'post-grad', 'professor', 'teaching-assistant'];
 
+    // These are the roles allowed to be added during registeration
     public const ALLOWED_USER_TYPES = ['employee', 'student', 'post-grad', 'professor', 'teaching-assistant'];
 
     public const LANGUAGE_DIRECTIONS = ['ltr', 'rtl'];
+
+    // This will be updated to have more roles
+    public const SPECIAL_ROLES = ['president', 'vice', 'secretary', 'dean'];
 }
